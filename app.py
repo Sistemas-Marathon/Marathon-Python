1b825b00aa92e7c047e8fde8aa09ff9abd5c15ce
santiago <sosasantiagottc@gmail.com>
2025-06-06 09:53:36 -0300

    Cambio

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 # app.py
 import streamlit as st
 import reposicion_cruzada
-import otroscript
-
 st.sidebar.title("🧭 Menú")
 opcion = st.sidebar.radio("Elegí un script:", ["Reposición Cruzada", "Otro Script"])
 
 if opcion == "Reposición Cruzada":
     reposicion_cruzada.run()
-elif opcion == "Otro Script":
-    otroscript.run()
