2a70a1e32a35d1a8a8cf449c619348e40a50933b
Santiago Sosa <74780210+santisosa19@users.noreply.github.com>
2026-03-23 14:19:34 -0300

    Refactor SQL query formatting in rrhh_martin.py

--- rrhh_martin.py
+++ rrhh_martin.py
@@ -24,31 +24,31 @@ def run():
 
             # Consulta
              query = """SELECT 
-                            GL_DATEPIECE AS FECHA,
-                            GL_REPRESENTANT AS COMERCIAL,
-                            GCL_LIBELLE AS NOMBRE_COMERCIAL,
-                            GCL_TCONTRAT AS MOD_HORARIA,
-                            CC_LIBELLE AS ROL_COMERCIAL,
-                            GL_ETABLISSEMENT AS ESTABLECIMIENTO,
-                            GP_TYPECOMPTA AS TIPO_DOCUMENTO,
-                            GL_NUMERO AS NUMERO,
-                            GL_FAMILLENIV1 AS TIPO_ARTICULO,
-                            GL_LIBREART2 AS MARCA, 
-                            GL_CODEARTICLE AS CODIGO_ARTICULO,
-                            GL_QTEFACT AS CANTIDAD,
-                            GL_TOTALTTC AS PRECIO
-                        FROM [MARAPROD24].[dbo].[GCLIGNEARTDIM] 
-                        INNER JOIN COMMERCIAL ON GL_REPRESENTANT=GCL_COMMERCIAL
-                        LEFT JOIN CHOIXCOD ON GCL_FONCTIONCOM=CC_CODE AND CC_TYPE='MFO'
-                        WHERE GL_DATEPIECE >= ? AND GL_DATEPIECE <= ?
-                        AND GL_NATUREPIECEG IN ('FFO')
-                        AND GP_TYPECOMPTA IN ('FAC','TIC')
-                        AND GL_TYPEARTICLE IN ('MAR','NOM')
-                        AND GP_TICKETANNULE <> 'X'
-                        AND GL_CODEARTICLE <> 'CE001'
-                        AND GL_CODEARTICLE <> 'NCPROMOCION'
-                        AND GL_FAMILLENIV1 <> 'INS'
-                        ORDER BY GL_DATEPIECE desc
+    GL_DATEPIECE AS FECHA,
+    GL_REPRESENTANT AS COMERCIAL,
+    GCL_LIBELLE AS NOMBRE_COMERCIAL,
+    GCL_TCONTRAT AS MOD_HORARIA,
+    CC_LIBELLE AS ROL_COMERCIAL,
+    GL_ETABLISSEMENT AS ESTABLECIMIENTO,
+    GP_TYPECOMPTA AS TIPO_DOCUMENTO,
+    GL_NUMERO AS NUMERO,
+    GL_FAMILLENIV1 AS TIPO_ARTICULO,
+    GL_LIBREART2 AS MARCA, 
+    GL_CODEARTICLE AS CODIGO_ARTICULO,
+    GL_QTEFACT AS CANTIDAD,
+    GL_TOTALTTC AS PRECIO
+FROM [MARAPROD24].[dbo].[GCLIGNEARTDIM] 
+INNER JOIN COMMERCIAL ON GL_REPRESENTANT=GCL_COMMERCIAL
+LEFT JOIN CHOIXCOD ON GCL_FONCTIONCOM=CC_CODE AND CC_TYPE='MFO'
+WHERE GL_DATEPIECE >= ? AND GL_DATEPIECE <= ?
+AND GL_NATUREPIECEG IN ('FFO')
+AND GP_TYPECOMPTA IN ('FAC','TIC')
+AND GL_TYPEARTICLE IN ('MAR','NOM')
+AND GP_TICKETANNULE <> 'X'
+AND GL_CODEARTICLE <> 'CE001'
+AND GL_CODEARTICLE <> 'NCPROMOCION'
+AND GL_FAMILLENIV1 <> 'INS'
+ORDER BY GL_DATEPIECE desc
             """
 
             df = pd.read_sql(query, conn, params=[fecha_inicio, fecha_fin])
